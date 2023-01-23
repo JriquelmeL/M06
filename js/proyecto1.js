e23a819f8df76a65b69e7b927de2fb000b89fe38
JriquelmeL <113889948+JriquelmeL@users.noreply.github.com>
2023-01-23 18:58:59 +0100

    Add files via upload

---
+++ js/proyecto1.js
@@ -0,0 +1,27 @@
+<html>
+
+<body>
+    <style>
+        h1{
+  color: green;
+}
+body{
+  background-color: yellow;
+}
+    </style>
+<h1>
+Probando JsFiddle
+</h1>
+<p>
+Saludame !
+</p>
+<button onclick="Saludo()"> Pulsar aqui
+</button>
+<script>
+function Saludo() {
+alert("Hola mundo!")
+}
+</script>
+</body>
+
+</html>
